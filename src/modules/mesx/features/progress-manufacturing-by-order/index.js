458ac8719b3cf5823b9a552204ace5f8d20d2480
DOAN Khuc Van (VTI.SnP) <doan.khucvan@vti.com.vn>
2022-04-25 23:37:15 +0700

    Feature/mesx progress manufacturing by order

---
+++ src/modules/mesx/features/progress-manufacturing-by-order/index.js
@@ -0,0 +1,257 @@
+import React, { useEffect } from 'react'
+
+import { Button } from '@mui/material'
+import { Box } from '@mui/system'
+import { useTranslation } from 'react-i18next'
+import { useHistory } from 'react-router-dom'
+
+import { useQueryState } from '~/common/hooks'
+import DataTable from '~/components/DataTable'
+import Page from '~/components/Page'
+import Status from '~/components/Status'
+import { ROUTE as ROUTEDATABASE } from '~/modules/database/routes/config'
+import {
+  convertFilterParams,
+  convertSortParams,
+  convertUtcDateToLocalTz,
+} from '~/utils'
+
+import { PROGRESS_MANUFACTURING_BY_ORDER_STATUS_OPTION } from '../../constants'
+import useProgressManufacturingByOrder from '../../redux/hooks/useProgressManufacturingByOrder'
+import { ROUTE } from '../../routes/config'
+import ProgressManufacturingFilter from './filter-form'
+const breadcrumbs = [
+  { title: 'statistical' },
+  {
+    route: ROUTE.PROGRESS_MANUFACTURING_BY_ORDER.LIST.PATH,
+    title: ROUTE.PROGRESS_MANUFACTURING_BY_ORDER.LIST.TITLE,
+  },
+]
+const progressManufacturingByOrder = () => {
+  const {
+    page,
+    keyword,
+    pageSize,
+    sort,
+    setPage,
+    setPageSize,
+    setSort,
+    filters,
+    setFilters,
+    setKeyword,
+  } = useQueryState()
+  const history = useHistory()
+  const {
+    data: { isLoading, progressByOrderList },
+    actions,
+  } = useProgressManufacturingByOrder()
+
+  useEffect(() => {
+    refreshData()
+  }, [keyword, page, pageSize, filters, sort])
+  const { t } = useTranslation(['mesx'])
+  const renderHeaderRight = () => {
+    // @TODO: <doan.khucvan> handle export
+    return (
+      <Button variant="outlined" disabled icon="download">
+        {t('progressManufacturingByOrder.export')}
+      </Button>
+    )
+  }
+  const columns = [
+    {
+      field: 'soIds',
+      headerName: t('progressManufacturingByOrder.code'),
+      width: 100,
+      sortable: true,
+      filterFormat: 'multiple',
+      fixed: true,
+      renderCell: (params) => {
+        const { row } = params
+        return (
+          <Button
+            variant="text"
+            onClick={() =>
+              history.push(
+                ROUTEDATABASE.SALE_ORDER.DETAILS.PATH.replace(
+                  ':id',
+                  `${row?.saleOrder?.id}`,
+                ),
+              )
+            }
+            size="small"
+          >
+            {row?.saleOrder?.code}
+          </Button>
+        )
+      },
+    },
+    {
+      field: 'name',
+      headerName: t('progressManufacturingByOrder.name'),
+      width: 100,
+      sortable: true,
+      fixed: true,
+      renderCell: (params) => {
+        const { row } = params
+        return row?.saleOrder?.name
+      },
+    },
+    {
+      field: 'orderedAt',
+      headerName: t('progressManufacturingByOrder.dateSell'),
+      width: 150,
+      filterFormat: 'date',
+      sortable: true,
+      renderCell: (params) => {
+        const { row } = params
+        return convertUtcDateToLocalTz(row?.saleOrder?.orderedAt)
+      },
+    },
+    {
+      field: 'deadline',
+      headerName: t('progressManufacturingByOrder.deliveryDate'),
+      width: 150,
+      sortable: true,
+      filterFormat: 'date',
+      renderCell: (params) => {
+        const { row } = params
+        return convertUtcDateToLocalTz(row?.saleOrder?.deadline)
+      },
+    },
+    {
+      field: 'status',
+      headerName: t('progressManufacturingByOrder.status'),
+      width: 150,
+      sortable: true,
+      fixed: true,
+      renderCell: (params) => {
+        const { status } = params.row?.saleOrder
+        return (
+          <Status
+            options={PROGRESS_MANUFACTURING_BY_ORDER_STATUS_OPTION}
+            value={status}
+            variant="text"
+          />
+        )
+      },
+    },
+    {
+      field: 'masterPlanIds',
+      headerName: t('progressManufacturingByOrder.planManufacturing'),
+      width: 200,
+      sortable: true,
+      renderCell: (params) => {
+        const { row } = params
+        return row?.masterPlan?.name
+      },
+    },
+    {
+      field: 'moName',
+      headerName: t('progressManufacturingByOrder.moName'),
+      width: 100,
+      sortable: true,
+      renderCell: (params) => {
+        const { row } = params
+        return row.code
+      },
+    },
+    {
+      field: 'planDate',
+      headerName: t('progressManufacturingByOrder.datePlan'),
+      width: 200,
+      sortable: true,
+      align: 'left',
+      renderCell: (params) => {
+        const { row } = params
+        return `${convertUtcDateToLocalTz(
+          row?.planFrom,
+        )} - ${convertUtcDateToLocalTz(row?.planTo)}`
+      },
+    },
+    {
+      field: 'datePerform',
+      headerName: t('progressManufacturingByOrder.datePerform'),
+      width: 150,
+      sortable: true,
+      renderCell: (params) => {
+        const { row } = params
+        return convertUtcDateToLocalTz(row?.masterPlan?.dateFrom)
+      },
+    },
+    {
+      field: 'dateEnd',
+      headerName: t('progressManufacturingByOrder.dateEnd'),
+      width: 150,
+      sortable: true,
+      renderCell: (params) => {
+        const { row } = params
+        return convertUtcDateToLocalTz(row?.completedAt)
+      },
+    },
+    {
+      field: 'planQuantity',
+      headerName: t('progressManufacturingByOrder.planQuantity'),
+      width: 100,
+      sortable: true,
+      align: 'right',
+    },
+    {
+      field: 'actualQuantity',
+      headerName: t('progressManufacturingByOrder.quantity'),
+      width: 100,
+      sortable: true,
+      align: 'right',
+    },
+  ]
+
+  const refreshData = () => {
+    const params = {
+      keyword: keyword.trim(),
+      page: page,
+      limit: pageSize,
+      filter: convertFilterParams(filters, [
+        ...columns,
+        {
+          field: 'soIds',
+        },
+        {
+          field: 'isHasPlan',
+        },
+      ]),
+      sort: convertSortParams(sort),
+    }
+    actions.getProgressManufacturingByOrder(params)
+  }
+
+  return (
+    <Page
+      breadcrumbs={breadcrumbs}
+      title={t('menu.progressManufacturingByOrder')}
+      onSearch={setKeyword}
+      placeholder={t('progressManufacturingByOrder.searchPlaceholder')}
+      loading={isLoading}
+      renderHeaderRight={renderHeaderRight}
+    >
+      <ProgressManufacturingFilter setFilters={setFilters} />
+      <Box mt={4}>
+        <DataTable
+          title={t('progressManufacturingByOrder.title')}
+          rows={progressByOrderList?.items}
+          pageSize={pageSize}
+          page={page}
+          columns={columns}
+          onPageChange={setPage}
+          onPageSizeChange={setPageSize}
+          onSortChange={setSort}
+          onFilterChange={setFilters}
+          total={progressByOrderList?.meta?.total}
+          sort={sort}
+          hideSetting
+        />
+      </Box>
+    </Page>
+  )
+}
+
+export default progressManufacturingByOrder
---
+++ src/modules/mesx/redux/reducers/progress-manudacturing-by-order.js
@@ -0,0 +1,36 @@
+import {
+  GET_PROGRESS_MANUFACTURING_BY_ORDER_START,
+  GET_PROGRESS_MANUFACTURING_BY_ORDER_SUCCESS,
+  GET_PROGRESS_MANUFACTURING_BY_ORDERD_FAILED,
+} from '~/modules/mesx/redux/actions/progress-by-order'
+const initialState = {
+  isLoading: false,
+  progressByOrderList: [],
+  total: null,
+}
+export default function progressManufacturingByOrder(
+  state = initialState,
+  action,
+) {
+  switch (action.type) {
+    case GET_PROGRESS_MANUFACTURING_BY_ORDER_START:
+      return {
+        ...state,
+        isLoading: true,
+      }
+    case GET_PROGRESS_MANUFACTURING_BY_ORDER_SUCCESS:
+      return {
+        ...state,
+        progressByOrderList: action.payload,
+        isLoading: false,
+        total: action.payload.total,
+      }
+    case GET_PROGRESS_MANUFACTURING_BY_ORDERD_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+      }
+    default:
+      return state
+  }
+}
