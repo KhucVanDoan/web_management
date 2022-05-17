2dc256b2200c8a23137ef9e24c08a959a2d5c383
lethemanh <manh.le@fullertonhealth.com>
2022-05-17 16:55:24 +0700

    Refresh work order list after autocomplete

--- src/modules/mesx/features/work-order/list/index.js
+++ src/modules/mesx/features/work-order/list/index.js
@@ -300,6 +300,7 @@ const WorkOrder = () => {
           ...isExecutingAutocompleteWorkOrder,
           [id]: false
         })
+        refreshData()
       },
       () => {
         setIsExecutingAutocompleteWorkOrder({
