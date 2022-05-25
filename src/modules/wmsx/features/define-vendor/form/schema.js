bb7b4ef43f4d08fee912a2e5c26fc634a9911d4b
DOAN Khuc Van (VTI.SnP) <doan.khucvan@vti.com.vn>
2022-05-25 23:39:56 +0700

    bugfix/wmsx-define-vendor

--- src/modules/wmsx/features/define-vendor/form/schema.js
+++ src/modules/wmsx/features/define-vendor/form/schema.js
@@ -1,9 +1,13 @@
 import * as Yup from 'yup'
 
+import { phoneSchema } from '~/common/schemas'
+
 const defineVendorSchema = (t) =>
   Yup.object().shape({
     code: Yup.string().required(t('general:form.required')),
     name: Yup.string().required(t('general:form.required')),
+    email: Yup.string().required(t('general:form.required')),
+    phone: phoneSchema(t),
   })
 
 export default defineVendorSchema
