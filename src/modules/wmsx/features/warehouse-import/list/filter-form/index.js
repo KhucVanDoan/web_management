ef99978bf5c41cd295c63cd63c983d2ef8645be7
anh.nth <anh.nguyenthihai@vti.com.vn>
2022-05-31 14:00:09 +0700

    bugfix/wmsx-warehouse-import

--- src/modules/wmsx/features/warehouse-import/list/filter-form/index.js
+++ src/modules/wmsx/features/warehouse-import/list/filter-form/index.js
@@ -3,8 +3,12 @@ import React, { useEffect } from 'react'
 import { Grid, createFilterOptions } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 
+import { TEXTFIELD_REQUIRED_LENGTH } from '~/common/constants'
 import { Field } from '~/components/Formik'
-import { WAREHOUSE_IMPORT_STATUS_OPTIONS } from '~/modules/wmsx/constants'
+import {
+  ORDER_STATUS_OPTIONS,
+  WAREHOUSE_IMPORT_STATUS_OPTIONS,
+} from '~/modules/wmsx/constants'
 import useDefineWarehouse from '~/modules/wmsx/redux/hooks/useDefineWarehouse'
 
 const FilterForm = () => {
@@ -21,8 +25,45 @@ const FilterForm = () => {
   return (
     <Grid container rowSpacing={4 / 3}>
       <Grid item xs={12}>
+        <Field.TextField
+          name="code"
+          label={t('movements.importExport.movementCode')}
+          placeholder={t('movements.importExport.movementCode')}
+          inputProps={{
+            maxLength: TEXTFIELD_REQUIRED_LENGTH.COMMON.MAX,
+          }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Field.DateRangePicker
+          name="createdAt"
+          label={t('movements.importExport.movementDate')}
+          placeholder={t('movements.importExport.movementDate')}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Field.TextField
+          name="createdByUser"
+          label={t('movements.createdByUser')}
+          placeholder={t('movements.createdByUser')}
+          inputProps={{
+            maxLength: TEXTFIELD_REQUIRED_LENGTH.COMMON.MAX,
+          }}
+        />
+      </Grid>
+      <Grid item xs={12}>
         <Field.Autocomplete
-          name="warehouseName"
+          name="status"
+          label={t('movements.movementStatus')}
+          placeholder={t('movements.movementStatus')}
+          options={ORDER_STATUS_OPTIONS}
+          getOptionLabel={(opt) => (opt?.text ? t(opt?.text) : '')}
+          getOptionValue={(opt) => opt?.id?.toString()}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Field.Autocomplete
+          name="warehouseId"
           label={t('movements.importExport.warehouseName')}
           placeholder={t('movements.importExport.warehouseName')}
           options={warehouseList}
@@ -35,19 +76,12 @@ const FilterForm = () => {
       </Grid>
       <Grid item xs={12}>
         <Field.Autocomplete
-          name="orderType"
+          name="movementType"
           label={t('movements.importExport.orderType')}
           placeholder={t('movements.importExport.orderType')}
           options={WAREHOUSE_IMPORT_STATUS_OPTIONS}
           getOptionValue={(opt) => opt?.id}
           getOptionLabel={(opt) => t(opt?.text)}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Field.DateRangePicker
-          name="planDate"
-          label={t('movements.movementDateRange')}
-          placeholder={t('movements.movementDateRange')}
         />
       </Grid>
     </Grid>
