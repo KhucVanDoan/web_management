970b4b0d5bb4e12f89c20a0cd612547d42545694
PHU Ngo Van (VTI.D9) <phu.ngovan@vti.com.vn>
2022-06-03 00:14:30 +0700

    Bugfix/wmsx warehouse space report

--- src/modules/wmsx/features/warehouse-space-report/table/index.js
+++ src/modules/wmsx/features/warehouse-space-report/table/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography, Box } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 
 import { useClasses } from '~/themes'
@@ -15,6 +15,36 @@ const Table = (props) => {
   return (
     <>
       {data?.sectors?.map((item) => (
+        //                 <Grid
+        //                 container
+        //                 rowSpacing={4 / 3}
+        //                 columnSpacing={{ xl: 8, xs: 4 }}
+        //               >
+        //                 {item?.shelfs?.map((shelf) => (
+        //                 <Grid xs={4}>
+        //                   {shelf?.floors?.reverse().map((floor) => (
+        //                     // <Grid xs={4}>
+        //                     <Grid className={classes.colFloors}>
+        //                       <ProgressBar
+        //                         bgcolor={
+        //                           +floor.fullmentPercent === 100
+        //                             ? theme.palette.error.main
+        //                             : +floor.fullmentPercent > 70
+        //                             ? theme.palette.secondary.main
+        //                             : theme.palette.primary.main
+        //                         }
+        //                         completed={+floor.fullmentPercent}
+        //                         name={floor.position}
+        //                       />
+        //                     </Grid>
+        //                     // </Grid>
+        //                   ))}
+        //                 </Grid>
+        //               ))}
+        // <Grid item lg={6} xs={12}>
+
+        // </Grid>
+        // </Grid>
         <Grid container>
           <Grid xs={12}>
             <div className={classes.rangeTitle}>
@@ -22,30 +52,31 @@ const Table = (props) => {
                 {item?.name}
               </Typography>
             </div>
-            <Grid style={{ overflowX: 'auto' }} wrap={false} align="bottom">
+            <Box
+              style={{ overflowX: 'auto' }}
+              wrap={false}
+              align="bottom"
+              display="flex"
+            >
               {item?.shelfs?.map((shelf) => (
-                <Grid>
+                <Grid xs={4}>
                   {shelf?.floors?.reverse().map((floor) => (
-                    <Grid>
-                      <Grid className={classes.colFloors}>
-                        <ProgressBar
-                          bgcolor={
-                            +floor.fullmentPercent === 100
-                              ? theme.palette.error.main
-                              : +floor.fullmentPercent > 70
-                              ? theme.palette.secondary.main
-                              : theme.palette.primary.main
-                          }
-                          completed={+floor.fullmentPercent}
-                          name={floor.position}
-                        />
-                      </Grid>
-                    </Grid>
+                    <ProgressBar
+                      bgcolor={
+                        +floor.fullmentPercent === 100
+                          ? theme.palette.error.main
+                          : +floor.fullmentPercent > 70
+                          ? theme.palette.secondary.main
+                          : theme.palette.primary.main
+                      }
+                      completed={+floor.fullmentPercent}
+                      name={floor.name}
+                    />
                   ))}
                 </Grid>
               ))}
               {item?.fullmentPercent > 0 && (
-                <Grid>
+                <Grid xs={4}>
                   <div className={classes.sectorFullment}>
                     <span>
                       {item.fullmentPercent ? item.fullmentPercent + '%' : null}
@@ -53,13 +84,13 @@ const Table = (props) => {
                   </div>
                 </Grid>
               )}
-            </Grid>
+            </Box>
           </Grid>
         </Grid>
       ))}
       {data?.fullmentPercent > 0 && (
         <Grid sx={{ mt: 2 }}>
-          <Grid xs={24}>
+          <Grid xs={12}>
             <div className={classes.warehouseFullment}>
               <span>
                 {data?.fullmentPercent ? data.fullmentPercent + '%' : null}
