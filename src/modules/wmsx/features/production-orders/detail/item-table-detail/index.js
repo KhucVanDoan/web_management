d16ca9a0d30f7d183ddb6124721bbcb4382fea2f
LINH Ta Quang (VTI.SnP) <linh.taquang@vti.com.vn>
2022-06-03 00:12:31 +0700

    Bugfix/wmsx production orders

---
+++ src/modules/wmsx/features/production-orders/detail/item-table-detail/index.js
@@ -0,0 +1,182 @@
+import React, { useEffect } from 'react'
+
+import { Checkbox } from '@mui/material'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { useTranslation } from 'react-i18next'
+
+import DataTable from '~/components/DataTable'
+import { useCommonManagement } from '~/modules/mesx/redux/hooks/useCommonManagement'
+import { convertUtcDateToLocalTz } from '~/utils'
+
+function ItemSettingTableDetail(props) {
+  const { t } = useTranslation(['wmsx'])
+  const { items } = props
+  const {
+    data: { itemList, warehouseList },
+    actions,
+  } = useCommonManagement()
+
+  useEffect(() => {
+    actions.getItems({})
+    actions.getWarehouses({})
+    actions.getBoms({ isGetAll: 1 })
+  }, [])
+  const getItemObject = (id) => {
+    return itemList?.find((item) => item?.id === id)
+  }
+
+  const getWarehouseTypeNames = (warehouseId) => {
+    const warehouse = warehouseList?.find((item) => item?.id === warehouseId)
+    return warehouse?.warehouseTypes?.length > 0
+      ? warehouse?.warehouseTypes
+          ?.map((warehouseType) => warehouseType?.name)
+          ?.join(', ')
+      : ''
+  }
+  const columns = [
+    {
+      field: 'id',
+      headerName: '#',
+      width: 80,
+
+      renderCell: (_, index) => {
+        return index + 1
+      },
+    },
+    {
+      field: 'code',
+      headerName: t('productionOrder.item.name'),
+      width: 250,
+      renderCell: (params) => {
+        const { itemId } = params.row
+        return <>{getItemObject(itemId)?.name || ''}</>
+      },
+    },
+    {
+      field: 'itemName',
+      headerName: t('productionOrder.item.code'),
+      width: 180,
+      renderCell: (params) => {
+        const itemId = params.row?.itemId
+        return <>{getItemObject(itemId)?.code || ''}</>
+      },
+    },
+    {
+      field: 'itemType',
+      headerName: t('productionOrder.item.type'),
+      width: 180,
+
+      renderCell: (params) => {
+        const { itemId } = params.row
+        return <>{getItemObject(itemId)?.itemType?.name || ''}</>
+      },
+    },
+    {
+      field: 'warehouseName',
+      headerName: t('productionOrder.item.warehouseName'),
+      width: 180,
+
+      renderCell: (params) => {
+        const { warehouse } = params?.row
+        return warehouse?.name
+      },
+    },
+    {
+      field: 'warehouseType',
+      headerName: t('productionOrder.item.warehouseType'),
+      width: 180,
+      renderCell: (params) => {
+        const { warehouseId } = params.row
+
+        return <>{getWarehouseTypeNames(warehouseId) || ''}</>
+      },
+    },
+    {
+      field: 'lotNumber',
+      headerName: t('productionOrder.item.lotNumber'),
+      width: 180,
+      renderCell: (params) => {
+        return <>{params?.row?.lotNumber}</>
+      },
+    },
+    {
+      field: 'mfg',
+      headerName: t('productionOrder.item.mfg'),
+      width: 180,
+      renderCell: (params) => {
+        return <>{convertUtcDateToLocalTz(params?.row?.mfg)}</>
+      },
+    },
+    {
+      field: 'packageId',
+      headerName: t('productionOrder.item.packageCode'),
+      width: 180,
+      renderCell: (params) => {
+        return <>{params?.row?.packageId}</>
+      },
+    },
+    {
+      field: 'quantity',
+      headerName: t('productionOrder.item.quantity'),
+      width: 180,
+      renderCell: (params) => {
+        const { quantity } = params.row
+        return <>{+quantity}</>
+      },
+    },
+    {
+      field: 'unitType',
+      headerName: t('productionOrder.item.unitType'),
+      width: 180,
+
+      renderCell: (params) => {
+        const { itemId } = params.row
+        return <>{getItemObject(itemId)?.itemUnit?.name || ''}</>
+      },
+    },
+    {
+      field: 'qcCheck',
+      headerName: t('productionOrder.item.qcCheck'),
+      width: 180,
+      renderCell: (params) => {
+        const { qcCheck } = params.row
+        return <Checkbox checked={Boolean(qcCheck)} />
+      },
+    },
+    {
+      field: 'qcCriteriaId',
+      headerName: t('productionOrder.item.qcCriteria'),
+      width: 180,
+      renderCell: (params) => {
+        const { qcCriteriaId } = params.row
+        return qcCriteriaId
+      },
+    },
+  ]
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          mb: 2,
+        }}
+      >
+        <Typography variant="h4">
+          {t('productionOrder.itemsDetails')}
+        </Typography>
+      </Box>
+      <DataTable
+        rows={items}
+        columns={columns}
+        hideSetting
+        hideFooter
+        striped={false}
+      />
+    </>
+  )
+}
+
+export default ItemSettingTableDetail
