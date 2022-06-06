e7193bea7a0d7e9bc73e5a666b36eea69a72f7b9
YEN Nguyen Hai (VTI.SnP) <yen.nguyenhai@vti.com.vn>
2022-06-06 20:01:51 +0700

    bugfix/wmsx-warehouse-design-linting

--- src/modules/wmsx/features/warehouse-design/index.js
+++ src/modules/wmsx/features/warehouse-design/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 
-import { Grid, Paper } from '@mui/material'
-import { styled } from '@mui/styles'
+import {
+  Grid,
+  //  Paper
+} from '@mui/material'
+// import { styled } from '@mui/styles'
 import { useTranslation } from 'react-i18next'
-import { useHistory } from 'react-router-dom'
+// import { useHistory } from 'react-router-dom'
 
 import { useQueryState } from '~/common/hooks'
 import Page from '~/components/Page'
@@ -21,19 +24,19 @@ const breadcrumbs = [
     title: ROUTE.WAREHOUSE_DESIGN.TITLE,
   },
 ]
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}))
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }))
 
 const warehouseDesign = () => {
   const { t } = useTranslation('wmsx')
   const { setKeyword } = useQueryState()
   const [warehouseRaio, setWarehouseRatio] = useState(1)
-  const [isLoading, setIsLoading] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
   const renderHeaderRight = () => {
     return <></>
   }
@@ -48,7 +51,7 @@ const warehouseDesign = () => {
       renderHeaderRight={renderHeaderRight}
       onSearch={setKeyword}
       placeholder={t('warehouseArea.searchPlaceholder')}
-      loading={isLoading}
+      // loading={isLoading}
     >
       <Grid container spacing={2}>
         <Grid item xs={4}>
