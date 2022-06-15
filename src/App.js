31adf560456d62780c94b41d05b9b9c4f1d0faf5
tan.nguyenminh <tan.nguyenminh@vti.com.vn>
2022-06-15 13:50:09 +0700

    debug ci.cd

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import { getLocale } from './utils'
 import { DateFns } from './utils/date-time'
 
 function App() {
+  console.log('aaaa')
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
