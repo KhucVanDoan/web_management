9050ec2e10d98cdfdcf0a2e6a9600e2ffd99f1cb
YEN Nguyen Hai (VTI.SnP) <yen.nguyenhai@vti.com.vn>
2022-06-21 00:32:24 +0700

    feature/mmsx-init

---
+++ src/modules/mmsx/routes/index.js
@@ -0,0 +1,15 @@
+import Dashboard from '~/modules/mmsx/features/dashboard'
+
+import { ROUTE } from './config'
+
+const routes = [
+  {
+    name: ROUTE.DASHBOARD.TITLE,
+    path: ROUTE.DASHBOARD.PATH,
+    component: Dashboard,
+    icon: 'home',
+    isInSidebar: true,
+  },
+]
+
+export default routes
--- src/store/saga.js
+++ src/store/saga.js
@@ -4,6 +4,7 @@ import authSagas from '~/modules/auth/redux/sagas'
 import configurationSagas from '~/modules/configuration/redux/sagas'
 import databaseSagas from '~/modules/database/redux/sagas'
 import mesxSagas from '~/modules/mesx/redux/sagas'
+import mmsxSagas from '~/modules/mmsx/redux/sagas'
 import qmsxSagas from '~/modules/qmsx/redux/sagas'
 import sharedSagas from '~/modules/shared/redux/sagas'
 import wmsxSagas from '~/modules/wmsx/redux/sagas'
@@ -16,4 +17,5 @@ export default function* rootSagas() {
   yield fork(wmsxSagas)
   yield fork(configurationSagas)
   yield fork(databaseSagas)
+  yield fork(mmsxSagas)
 }
