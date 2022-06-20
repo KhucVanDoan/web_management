880eadb64b039dd5d21f255d5c76fd59c4b613ef
LINH Ta Quang (VTI.SnP) <linh.taquang@vti.com.vn>
2022-06-20 23:15:28 +0700

    feature/wmsx-vendor-cr

--- src/modules/wmsx/features/define-vendor/detail/index.js
+++ src/modules/wmsx/features/define-vendor/detail/index.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react'
 
 import { Grid } from '@mui/material'
+import Box from '@mui/material/Box'
 import { useTranslation } from 'react-i18next'
 import { useParams, useHistory } from 'react-router-dom'
 
+import { MODAL_MODE } from '~/common/constants'
 import ActionBar from '~/components/ActionBar'
+import Button from '~/components/Button'
 import LV from '~/components/LabelValue'
 import Page from '~/components/Page'
+import Tabs from '~/components/Tabs'
 import TextField from '~/components/TextField'
 import useDefineVendor from '~/modules/wmsx/redux/hooks/useDefineVendor'
 import { ROUTE } from '~/modules/wmsx/routes/config'
+
+import SupperCapacity from '../capacity'
+import TransactionVendor from '../transaction'
 
 const breadcrumbs = [
   {
@@ -29,6 +36,7 @@ const DefineVendorDetail = () => {
   const { t } = useTranslation(['wmsx'])
   const history = useHistory()
   const { id } = useParams()
+  const mode = MODAL_MODE.DETAIL
   const {
     data: { isLoading, vendorDetails },
     actions,
@@ -45,54 +53,102 @@ const DefineVendorDetail = () => {
     history.push(ROUTE.DEFINE_VENDEOR.LIST.PATH)
   }
 
+  const renderHeaderRight = () => {
+    return (
+      <>
+        <Button
+          onClick={() => history.push(ROUTE.DEFINE_ITEM.CREATE.PATH)}
+          sx={{ ml: 4 / 3 }}
+          icon="add"
+        >
+          {t('defineVendor.updateStatusPayment')}
+        </Button>
+      </>
+    )
+  }
   return (
     <Page
       breadcrumbs={breadcrumbs}
       title={t('menu.defineVendorDetail')}
       onBack={backToList}
       loading={isLoading}
+      renderHeaderRight={renderHeaderRight}
     >
       <Grid container justifyContent="center">
         <Grid item xl={11} xs={12}>
-          <Grid container rowSpacing={4 / 3} columnSpacing={{ xl: 8, xs: 4 }}>
-            <Grid item lg={6} xs={12}>
-              <LV label={t('defineVendor.code')} value={vendorDetails.code} />
-            </Grid>
-            <Grid item lg={6} xs={12}>
-              <LV label={t('defineVendor.name')} value={vendorDetails.name} />
-            </Grid>
-            <Grid item lg={6} xs={12}>
-              <LV
-                label={t('defineVendor.address')}
-                value={vendorDetails.address}
-              />
-            </Grid>
-            <Grid item lg={6} xs={12}>
-              <LV label={t('defineVendor.phone')} value={vendorDetails.phone} />
-            </Grid>
-            <Grid item lg={6} xs={12}>
-              <LV label={t('defineVendor.fax')} value={vendorDetails.fax} />
-            </Grid>
-            <Grid item lg={6} xs={12}>
-              <LV label={t('defineVendor.email')} value={vendorDetails.email} />
-            </Grid>
+          <Tabs
+            list={[
+              t('defineVendor.commonInfo'),
+              t('defineVendor.supplierCapacity'),
+              t('defineVendor.transaction'),
+            ]}
+          >
+            {/* tab 1 */}
+            <Box>
+              <Grid
+                container
+                rowSpacing={4 / 3}
+                columnSpacing={{ xl: 8, xs: 4 }}
+              >
+                <Grid item lg={6} xs={12}>
+                  <LV
+                    label={t('defineVendor.code')}
+                    value={vendorDetails.code}
+                  />
+                </Grid>
+                <Grid item lg={6} xs={12}>
+                  <LV
+                    label={t('defineVendor.name')}
+                    value={vendorDetails.name}
+                  />
+                </Grid>
+                <Grid item lg={6} xs={12}>
+                  <LV
+                    label={t('defineVendor.address')}
+                    value={vendorDetails.address}
+                  />
+                </Grid>
+                <Grid item lg={6} xs={12}>
+                  <LV
+                    label={t('defineVendor.phone')}
+                    value={vendorDetails.phone}
+                  />
+                </Grid>
+                <Grid item lg={6} xs={12}>
+                  <LV label={t('defineVendor.fax')} value={vendorDetails.fax} />
+                </Grid>
+                <Grid item lg={6} xs={12}>
+                  <LV
+                    label={t('defineVendor.email')}
+                    value={vendorDetails.email}
+                  />
+                </Grid>
 
-            <Grid item xs={12}>
-              <TextField
-                name="description"
-                label={t('defineVendor.description')}
-                multiline
-                rows={3}
-                value={vendorDetails.description}
-                readOnly
-                sx={{
-                  'label.MuiFormLabel-root': {
-                    color: (theme) => theme.palette.subText.main,
-                  },
-                }}
-              />
-            </Grid>
-          </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="description"
+                    label={t('defineVendor.description')}
+                    multiline
+                    rows={3}
+                    value={vendorDetails.description}
+                    readOnly
+                    sx={{
+                      'label.MuiFormLabel-root': {
+                        color: (theme) => theme.palette.subText.main,
+                      },
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
+            {/* tab 2 */}
+            <SupperCapacity
+              mode={mode}
+              vendorAbilities={vendorDetails?.vendorAbilities}
+            />
+            {/* tab 3 */}
+            <TransactionVendor />
+          </Tabs>
           <ActionBar onBack={backToList} />
         </Grid>
       </Grid>
---
+++ src/modules/wmsx/features/define-vendor/transaction/index.js
@@ -0,0 +1,194 @@
+import { useEffect } from 'react'
+
+import { IconButton } from '@mui/material'
+import { first } from 'lodash'
+import { useTranslation } from 'react-i18next'
+import { useHistory } from 'react-router-dom'
+
+import { useQueryState } from '~/common/hooks'
+import DataTable from '~/components/DataTable'
+import Icon from '~/components/Icon'
+import usePurchasedOrder from '~/modules/database/redux/hooks/usePurchasedOrder'
+import { ROUTE } from '~/modules/database/routes/config'
+import useDefineVendor from '~/modules/wmsx/redux/hooks/useDefineVendor'
+import {
+  convertFilterParams,
+  convertSortParams,
+  convertUtcDateTimeToLocalTz,
+} from '~/utils'
+
+import FilterForm from './filter'
+
+function TransactionVendor() {
+  const { t } = useTranslation(['wmsx'])
+  const history = useHistory()
+  const DEFAULT_FILTERS = {
+    code: '',
+    name: '',
+    status: '',
+    createdAt: null,
+    deadline: null,
+  }
+  const {
+    page,
+    pageSize,
+    sort,
+    filters,
+    keyword,
+    setPage,
+    setPageSize,
+    setSort,
+    setFilters,
+  } = useQueryState({
+    filters: DEFAULT_FILTERS,
+  })
+
+  const {
+    data: { purchasedOrderList, total },
+    actions,
+  } = usePurchasedOrder()
+
+  const {
+    data: { vendorDetails },
+  } = useDefineVendor()
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: '#',
+      width: 80,
+      fixed: true,
+      renderCell: (_, index) => {
+        return index + 1
+      },
+    },
+    {
+      field: 'code',
+      headerName: t('mesx:purchasedOrder.code'),
+      width: 100,
+      sortable: true,
+      fixed: true,
+    },
+    {
+      field: 'name',
+      headerName: t('mesx:purchasedOrder.name'),
+      width: 180,
+      sortable: true,
+      fixed: true,
+    },
+    {
+      field: 'purchasedAt',
+      headerName: t('mesx:purchasedOrder.purchasedAt'),
+      width: 150,
+      sortable: true,
+      filterFormat: 'date',
+      renderCell: (params) => {
+        const { purchasedAt } = params.row
+        return convertUtcDateTimeToLocalTz(purchasedAt)
+      },
+    },
+    {
+      field: 'deadline',
+      headerName: t('mesx:purchasedOrder.deadline'),
+      width: 150,
+      sortable: true,
+      filterFormat: 'date',
+      renderCell: (params) => {
+        const { deadline } = params.row
+        return convertUtcDateTimeToLocalTz(deadline)
+      },
+    },
+    {
+      field: 'totalPrice',
+      headerName: t('mesx:purchasedOrder.totalPrice'),
+      width: 150,
+      renderCell: (params) => {
+        const { price, quantity } = params?.row?.purchasedOrderDetail[0]
+        return price * quantity
+      },
+    },
+    {
+      field: 'statusPay',
+      headerName: t('mesx:purchasedOrder.payStatus'),
+      width: 150,
+    },
+    {
+      field: 'customer',
+      headerName: t('mesx:purchasedOrder.customer.subTitle'),
+      width: 150,
+      sortable: false,
+      renderCell: (params) => {
+        const { vendorName } = params?.row
+        return vendorName
+      },
+    },
+    {
+      field: 'action',
+      headerName: t('general:common.action'),
+      align: 'center',
+      renderCell: (params) => {
+        const { id } = params.row
+        return (
+          <IconButton
+            onClick={() =>
+              history.push(
+                ROUTE.PURCHASED_ORDER.DETAIL.PATH.replace(':id', `${id}`),
+              )
+            }
+          >
+            <Icon name="show" />
+          </IconButton>
+        )
+      },
+    },
+  ]
+
+  const refreshData = () => {
+    const params = {
+      keyword: keyword.trim(),
+      page,
+      limit: pageSize,
+      filter: convertFilterParams(
+        { ...filters, vendorName: vendorDetails.name },
+        columns,
+      ),
+      sort: convertSortParams(sort),
+    }
+
+    actions.searchPurchasedOrders(params)
+  }
+
+  useEffect(() => {
+    refreshData()
+  }, [page, pageSize, filters, sort, keyword])
+  const totalDebt = purchasedOrderList
+    ?.map((item) => first(item?.purchasedOrderDetail))
+    ?.reduce((acc, cur) => {
+      return acc + cur?.quantity * cur?.price
+    }, 0)
+    .toFixed(0)
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+
+  return (
+    <DataTable
+      title={`Tổng công nợ: ${totalDebt} VNĐ`}
+      rows={purchasedOrderList}
+      pageSize={pageSize}
+      page={page}
+      columns={columns}
+      onPageChange={setPage}
+      onPageSizeChange={setPageSize}
+      onSortChange={setSort}
+      total={total}
+      filters={{
+        form: <FilterForm />,
+        defaultValue: DEFAULT_FILTERS,
+        values: filters,
+        onApply: setFilters,
+      }}
+      sort={sort}
+    />
+  )
+}
+
+export default TransactionVendor
