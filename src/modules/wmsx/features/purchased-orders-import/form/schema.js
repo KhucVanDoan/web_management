15596cf1c014eeeda834963ebebe729e1a8fb24f
DOAN Khuc Van (VTI.SnP) <doan.khucvan@vti.com.vn>
2022-06-22 19:23:22 +0700

    Bugfix/wmsx buglist doankv 2206

--- src/modules/wmsx/features/purchased-orders-import/form/schema.js
+++ src/modules/wmsx/features/purchased-orders-import/form/schema.js
@@ -1,6 +1,9 @@
 import * as Yup from 'yup'
 
-import { NUMBER_FIELD_REQUIRED_SIZE } from '~/common/constants'
+import {
+  NUMBER_FIELD_REQUIRED_SIZE,
+  TEXTFIELD_REQUIRED_LENGTH,
+} from '~/common/constants'
 
 export const schema = (t) =>
   Yup.object().shape({
@@ -16,16 +19,10 @@ export const schema = (t) =>
         lotNumber: Yup.string()
           .nullable()
           .required(t('general:form.required'))
-          .min(
-            NUMBER_FIELD_REQUIRED_SIZE.LOT_NUMBER.MIN,
-            t('general:form.minNumber', {
-              min: NUMBER_FIELD_REQUIRED_SIZE.LOT_NUMBER.MIN,
-            }),
-          )
-          .max(
-            NUMBER_FIELD_REQUIRED_SIZE.LOT_NUMBER.MAX,
-            t('general:form.maxNumber', {
-              max: NUMBER_FIELD_REQUIRED_SIZE.LOT_NUMBER.MAX,
+          .length(
+            TEXTFIELD_REQUIRED_LENGTH.CODE_10.MAX,
+            t('general:form.length', {
+              length: TEXTFIELD_REQUIRED_LENGTH.CODE_10.MAX,
             }),
           ),
         mfg: Yup.string().nullable().required(t('general:form.required')),
