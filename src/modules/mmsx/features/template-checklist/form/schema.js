e6d158711e07158d198050079214a92d72fecf96
anhnth <anh.nguyenthihai@vti.com.vn>
2022-07-26 17:38:03 +0700

    bugfix/mmsx-template-checklist-maxlength

--- src/modules/mmsx/features/template-checklist/form/schema.js
+++ src/modules/mmsx/features/template-checklist/form/schema.js
@@ -4,14 +4,7 @@ import { TEXTFIELD_REQUIRED_LENGTH } from '~/common/constants'
 
 export const validateShema = (t) =>
   Yup.object().shape({
-    code: Yup.string()
-      .required(t('general:form.required'))
-      .max(
-        TEXTFIELD_REQUIRED_LENGTH.CODE_7.MAX,
-        t('general:form.maxLength', {
-          max: TEXTFIELD_REQUIRED_LENGTH.CODE_7.MAX,
-        }),
-      ),
+    code: Yup.string().required(t('general:form.required')),
     name: Yup.string()
       .required(t('general:form.required'))
       .max(
