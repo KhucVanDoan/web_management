ba16c5e0f867a1e6806c95350df873ceb8c58405
YEN Nguyen Hai (VTI.SnP) <yen.nguyenhai@vti.com.vn>
2022-11-14 22:43:32 +0700

    feature/avenue-user-me

--- src/modules/auth/redux/reducers/auth.js
+++ src/modules/auth/redux/reducers/auth.js
@@ -1,5 +1,3 @@
-import { CHANGE_NOTIFICATION_STATUS_SUCCESS } from '~/modules/shared/redux/actions/notification'
-
 import {
   LOGIN_FAILED,
   LOGIN_START,
@@ -7,14 +5,10 @@ import {
   LOGOUT_FAILED,
   LOGOUT_START,
   LOGOUT_SUCCESS,
-  GET_USER_ME_START,
-  GET_USER_ME_SUCCESS,
-  GET_USER_ME_FAILED,
 } from '../actions/auth'
 
 const initialState = {
   isLoading: false,
-  userInfo: {},
 }
 
 /**
@@ -27,7 +21,6 @@ export default function auth(state = initialState, action) {
   switch (action.type) {
     case LOGIN_START:
     case LOGOUT_START:
-    case GET_USER_ME_START:
       return {
         ...state,
         isLoading: true,
@@ -36,32 +29,15 @@ export default function auth(state = initialState, action) {
       return {
         ...state,
         isLoading: false,
-        userInfo: {},
       }
     case LOGIN_SUCCESS:
       return {
         ...state,
         isLoading: false,
-        userInfo: action.payload,
-      }
-
-    case GET_USER_ME_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        userInfo: action.payload,
-      }
-
-    case CHANGE_NOTIFICATION_STATUS_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        userInfo: action.payload,
       }
 
     case LOGIN_FAILED:
     case LOGOUT_FAILED:
-    case GET_USER_ME_FAILED:
       return {
         ...state,
         isLoading: false,
