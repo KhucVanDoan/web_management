cd135852f890a853952013ea041fba47cc03a7ec
YEN Nguyen Hai (VTI.SnP) <yen.nguyenhai@vti.com.vn>
2023-01-30 23:12:12 +0700

    feature/avenue-login-bg

--- src/layouts/Auth/style.js
+++ src/layouts/Auth/style.js
@@ -1,4 +1,4 @@
-import BackgroundImg from '~/assets/images/auth/BackgroundIntroduction.png'
+import BackgroundImg from '~/assets/images/auth/BackgroundLogin.jpg'
 
 const style = (theme) => ({
   root: {
@@ -12,6 +12,21 @@ const style = (theme) => ({
     backgroundImage: `url(${BackgroundImg})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover',
+    position: 'relative',
+
+    '&:before': {
+      content: '""',
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      background: 'rgba(255, 255, 255, 0.3)',
+    },
+
+    '&>*': {
+      position: 'relative',
+    },
   },
   rightPanel: {
     flex: 1,
